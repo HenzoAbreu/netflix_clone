c18ae48c5bc75485072b7c1b35a6565cd21dafa8
Henzo <henzo.abreu2@gmail.com>
2021-10-22 12:08:57 -0300

    completed

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState} from 'react'
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
+import './App.css'
+import FeaturedMovie from './components/FeaturedMovie.js';
+import Header from './components/Header'
+
+export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
+
+  useEffect(()=>{
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList();
+      setMovieList(list)
+
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
+      let chosen = originals[0].items.results[randomChosen]
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo)
+    }
+
+    loadAll()
+  }, []);
+  useEffect(() => {
+    const scrollListener = () => {
+      if(window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener);
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener)
+    }
+  }, []);
+
+  return (
+    <div className="page">
+
+      <Header black={blackHeader} />
+
+      {featuredData && 
+        <FeaturedMovie item={featuredData} />
+      }
+
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+
+      <footer>
+        Feito em live: (https://youtu.be/tBweoUiMsDg), com intuito de estudar<br/>
+        Direitos de imagem para Netflix<br/>
+        Dados extraídos de Themoviedb.org
+      </footer>
+
+      {movieList.length <= 0 &&
+      <div className="loading">
+          <img src="https://cdn.lowgif.com/small/0534e2a412eeb281-the-counterintuitive-tech-behind-netflix-s-worldwide.gif" alt="Carregando" />
+      </div>
+      }
+    </div>
+    
+  );
+}
